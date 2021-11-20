aaa07175efd81baa04d04f2ad3cd892dded309b8
talgarbekov <talgarbekov84@gmail.com>
2021-11-20 15:37:24 +0000

    added sec_group

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "web" {
   ami                    = "ami-ae6272b8"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
   availability_zone      = "us-east-1a"
-  user_data              = file("userdata.sh")
+  user_data              = file ("userdata.sh")
 }
--- ec2/output.tf
+++ ec2/output.tf
@@ -1,19 +1,16 @@
-output "PIP" {
-  value = aws_instance.web.public_ip
-}
-
-output "AMI" {
-  value = aws_instance.web.ami
-}
-
 output "AZ" {
   value = aws_instance.web.availability_zone
 }
 
-output "aws_instance_region" {
-  value = "us-east-1"
+output "public_ip" {
+  value = aws_instance.web.public_ip
 }
 
-output "ID" {
+
+output "instance_id" {
   value = aws_instance.web.id
+}
+
+output "aws_instance_region" {
+  value = "us-east-1"
 }
